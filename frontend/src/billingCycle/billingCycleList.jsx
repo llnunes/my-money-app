0f9e8591e3a931af9ffdfca4fb47d5137e1a28f4
lucas <lucas.nunes@basis.com.br>
2019-01-06 20:27:44 -0200

    Evoluindo a aplicacao

---
+++ frontend/src/billingCycle/billingCycleList.jsx
@@ -0,0 +1,24 @@
+import React, {Component} from 'react'
+
+class BillingCycleList extends Component {
+    render () {
+        return (
+            <div>
+                <table className='table'>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Mes</th>
+                            <th>Ano</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                    </tbody>
+                </table>
+            </div>
+        )
+    }
+}
+
+export default BillingCycleList
